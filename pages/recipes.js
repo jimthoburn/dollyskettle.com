5c93700b85843cac35d61670ffcd4d2917b07665
Jim Thoburn <jim@tobbi.co>
2023-05-06 11:34:49 -0700

    Alphabetize the recipe list

--- pages/recipes.js
+++ pages/recipes.js
@@ -6,8 +6,26 @@ const    html = htm.bind(createElement);
 import { htmlDecode }   from "../helpers/html-decode.js";
 import { normalizeURL } from "../helpers/url.js";
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function compareTwoPostsByTitle(a, b) {
+  // a is less than b by some ordering criterion
+  if (a.title.rendered < b.title.rendered) {
+    return -1;
+  }
+
+  // a is greater than b by the ordering criterion
+  if (a.title.rendered > b.title.rendered) {
+    return 1;
+  }
+
+  // a must be equal to b
+  return 0;
+}
 
 function RecipesPage({ posts }) {
+
+  // Alphabetize the posts by title
+  const sortedPosts = posts.sort(compareTwoPostsByTitle);
 
   return html`
   <header>
@@ -18,7 +36,7 @@ function RecipesPage({ posts }) {
 
   <div class="alphabetical-recipe-list">
     <ul>
-      ${posts.map(post => {
+      ${sortedPosts.map(post => {
           return html`
           <li><a href="/${ normalizeURL(post.link) }/">${ htmlDecode(post.title.rendered) }</a></li>
           `;
