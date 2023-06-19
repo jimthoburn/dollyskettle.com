494aa6a2856558991a186e3f91856fdb381796c4
Jim Thoburn <jim@tobbi.co>
2023-06-19 10:11:27 -0700

    Make category pages friendly to slower connections

--- pages/category.js
+++ pages/category.js
@@ -7,7 +7,6 @@ import { htmlDecode }   from "../helpers/html-decode.js";
 import { normalizeURL } from "../helpers/url.js";
 import { getPostImage } from "../helpers/post.js";
 
-
 function CategoryPage({ title, posts, DOMParser }) {
 
   return html`
@@ -20,14 +19,18 @@ function CategoryPage({ title, posts, DOMParser }) {
   <div class="recipe-list">
     <ul>
       ${posts.map(post => {
-        // console.log("CategoryPage");
-        // console.log(post.title.rendered);
         const image = getPostImage({ post });
         if (image) {
           return html`
             <li class="has-image">
               <a href="/${ normalizeURL(post.link) }/">
-                <img src="${ getPostImage({ post }).src }" alt="" />
+                <img
+                  src="${ image.src }"
+                  width="${ image.width }"
+                  height="${ image.height }"
+                  alt=""
+                  loading="lazy"
+                />
                 ${ htmlDecode({ html: post.title.rendered, DOMParser }) }
               </a>
             </li>
