e21cd3d2da69d1298108df78c23b483182e921c1
Jim Thoburn <jim@tobbi.co>
2023-11-23 11:18:40 -0800

    Update `build` task for deno (part 3)

--- download-and-build.js
+++ download-and-build.js
@@ -3,6 +3,7 @@ import { load } from "dotenv/mod.ts";
 import { DOMParser } from "deno_dom/deno-dom-wasm.ts";
 import { refreshData, getPublicURLs, getMediaURLs } from "./data/post.js";
 import { build } from "./build.js";
+import { downloadImages } from "./download-images.js";
 
 // https://deno.land/std/dotenv/mod.ts
 const env = await load();
