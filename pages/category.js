c9d96fdbf4800b566862b45a2932d11deeadc448
Jim Thoburn <jim@tobbi.co>
2024-06-16 16:21:16 -0700

    Start with most recent posts for category pages

--- pages/category.js
+++ pages/category.js
@@ -7,7 +7,26 @@ import { htmlDecode }   from "../helpers/html-decode.js";
 import { normalizeURL } from "../helpers/url.js";
 import { getPostImage } from "../helpers/post.js";
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function orderByDateDescending(a, b) {
+  // b is less than a by some ordering criterion
+  if (b.date < a.date) {
+    return -1;
+  }
+
+  // b is greater than a by the ordering criterion
+  if (b.date > a.date) {
+    return 1;
+  }
+
+  // a must be equal to b
+  return 0;
+}
+
 function CategoryPage({ title, posts, DOMParser }) {
+
+  // Start with most recent posts
+  const sortedPosts = posts.sort(orderByDateDescending);
 
   return html`
   <header>
@@ -18,7 +37,7 @@ function CategoryPage({ title, posts, DOMParser }) {
 
   <div class="recipe-list">
     <ul>
-      ${posts.map(post => {
+      ${sortedPosts.map(post => {
         const image = getPostImage({ post });
         if (image) {
           return html`
